5b33bb53ef068490f794335f65a3423c4a5b75f5
Chase Jensen <soccerchase@gmail.com>
2018-12-10 10:58:34 -0700

    Switch statement

--- 2_js/02_switch/switch.js
+++ 2_js/02_switch/switch.js
@@ -1,1 +1,18 @@
 document.getElementById('rootnode').innerHTML = "SWITCH STATEMENTS";
+
+var x = 4;
+
+// cases are like a period at the end of a sentence
+switch(x) {
+  case 1:
+    console.log("The number is one");
+    break;
+  case 2:
+    console.log("The number is two");
+    break;
+  case 3:
+    console.log("The number is three");
+    break;
+  default:
+    console.log(`The number is ${x}`);
+}
