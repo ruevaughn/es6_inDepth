713a90ea970ab86a059dadb04d6e28aacf541896
Chase Jensen <soccerchase@gmail.com>
2018-12-22 11:51:29 -0700

    More Redux Reducer, Action, and Store

--- 10_react/redux/redux-store-reducer-action.js
+++ 10_react/redux/redux-store-reducer-action.js
@@ -7,8 +7,13 @@
 const reducer = (state = [], action) => {
   if (action.type === 'split_string') {
     return action.payload.split('');
+  } else if (action.type == 'add_character') {
+    // need to return completely new object (data structure), not mutate the data.
+    return [ ...state, action.payload ];
   }
-};
+
+  return state;
+}
 const store = Redux.createStore(reducer);
 
 store.getState();
@@ -19,5 +24,14 @@ const action = {
 };
 
 store.dispatch(action);
+
+store.getState();
+
+const action2 = {
+  type: 'add_character',
+  payload: 'a',
+};
+
+store.dispatch(action2);
 
 store.getState();
