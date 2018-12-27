667e145947e6c1143e8b09c100d36121faf64325
Chase Jensen <soccerchase@gmail.com>
2018-12-27 12:02:06 -0700

    Move Webpack and Babel application custom libraries to library folder

--- 03_es6/app/index.js
+++ 03_es6/app/index.js
@@ -1,6 +1,7 @@
-import { fellowship, total } from './fellowship';
-import multiply, { add } from './math';
-import { Animal, Lion } from './lionKing';
+import { fellowship, total } from './libraries/fellowship';
+import multiply, { add } from './libraries/math';
+import { Animal, Lion } from './libraries/lionKing';
+import Calculator from './libraries/calculator';
 
 console.log(fellowship);
 console.log(total);
@@ -17,3 +18,10 @@ king.hello();
 let son = new Lion('Simba', 2, "golden");
 console.log(son);
 son.hello();
+
+// Classes 2
+let m = Calculator.multiply(5, 7);
+console.log(m);
+
+let a = Calculator.add(5, 7);
+console.log(a);
---
+++ 03_es6/app/libraries/calculator.js
@@ -0,0 +1,11 @@
+class Calculator {
+  static multiply(a, b) {
+    return a*b;
+  }
+
+  static add(a, b) {
+    return a+b;
+  }
+}
+
+export default Calculator;
